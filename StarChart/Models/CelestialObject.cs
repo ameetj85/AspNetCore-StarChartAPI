f5a9a2778e911b3f03725f977340e570e42bd3b4
Ameet Jayawant <ameetj85@gmail.com>
2019-04-26 20:50:15 -0400

    bug fix

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,14 @@ namespace StarChart.Models
 {
     public class CelestialObject
     {
-        public int id { get; set; }
+        public int Id { get; set; }
 
         [Required]
-        public string name { get; set; }
+        public string Name { get; set; }
 
         public int? OrbitedObjectId { get; set; }
 
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
 
