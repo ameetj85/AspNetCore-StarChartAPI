7e85b67b802b0e0f45bf0662beb76773d562f0fc
Ameet Jayawant <ameetj85@gmail.com>
2019-04-26 20:47:34 -0400

    celestial object model

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        public int? OrbitedObjectId { get; set; }
+
+        public List<CelestialObject> Satellites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+
+    }
+}
